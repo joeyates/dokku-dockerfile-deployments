28d304f89d1205737e6cabf0edb1e05b34758dd6
Joe Yates <joe.g.yates@gmail.com>
2026-04-01 17:33:21 +0200

    Check URLs (with or without signing)

---
+++ imgproxy/bin/check-url.exs
@@ -0,0 +1,76 @@
+#!/usr/bin/env elixir
+
+# For the --url, choose an image path relative to REMOTE_HOST_FILESYSTEM_ROOT
+# bin/imgproxy-url --prefix "https://$APP_DOMAIN" --key "$IMGPROXY_KEY" --salt "$IMGPROXY_SALT" --path "/path/to/image.jpg"
+
+Mix.install([
+  {:helpful_options, "~> 0.3"},
+  {:imgproxy, "~> 3.1",
+   git: "https://github.com/bmuller/imgproxy.git", ref: "b181f2932b2f7f2448a3ff873d9b2b105d9a32ba"},
+  {:req, "~> 0.4"}
+])
+
+defmodule CheckImgproxyUrl do
+  @moduledoc false
+
+  @options_switches [
+    path: %{type: :string, required: true},
+    prefix: %{type: :string, required: true},
+    key: %{type: :string},
+    salt: %{type: :string}
+  ]
+
+  def run(args) do
+    {:ok, path, options} = parse_args(args)
+    :ok = set_config(options)
+    url = build_url(path)
+    :ok = check_url(url)
+  end
+
+  def parse_args(args) do
+    case HelpfulOptions.parse(args, switches: @options_switches) do
+      {:ok, parsed, []} ->
+        {path, options} = Map.pop(parsed, :path)
+        {:ok, path, options}
+
+      {:error, reason} ->
+        raise "Error: #{reason}"
+    end
+  end
+
+  def set_config(options) do
+    Application.put_env(:imgproxy, :prefix, options.prefix)
+
+    if options[:key] && options[:salt] do
+      Application.put_env(:imgproxy, :key, options.key)
+      Application.put_env(:imgproxy, :salt, options.salt)
+    end
+  end
+
+  defp build_url(path) do
+    base = URI.new!("local://")
+    uri = %{base | path: path}
+
+    uri
+    |> to_string()
+    |> Imgproxy.new()
+    |> Imgproxy.set_source_url_encoding(:plain)
+    |> Imgproxy.to_string()
+  end
+
+  def check_url(url) do
+    IO.puts("Checking URL: #{inspect(url)}")
+
+    case Req.get!(url) do
+      %Req.Response{status: 200} ->
+        IO.puts("URL is valid and accessible")
+        :ok
+
+      %Req.Response{} = response ->
+        raise "Unexpected response: #{inspect(response)}"
+    end
+  end
+end
+
+CheckImgproxyUrl.run(System.argv())
+System.halt(0)
